874e0c8fa20bdec80b8bf40a5493ee779d3df459
glenmark <coddybanks178@gmail.com>
2026-06-30 03:07:17 +0800

    Add MapLibre initialization in Application class and link MapLibre dependency to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.maplibre.android)
     debugImplementation(libs.androidx.compose.ui.tooling)
 
     // Hilt
--- app/src/main/java/com/ridecompanion/RideCompanionApp.kt
+++ app/src/main/java/com/ridecompanion/RideCompanionApp.kt
@@ -2,10 +2,13 @@ package com.ridecompanion
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import org.maplibre.android.MapLibre
+import org.maplibre.android.WellKnownTileServer
 
 @HiltAndroidApp
 class RideCompanionApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        MapLibre.getInstance(this, null, WellKnownTileServer.MapLibre)
     }
 }
